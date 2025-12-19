acf1bd3f44a3052cae5ecd28f8092d6ae753b075
Milan Bontridder <119121532+mbontrid@users.noreply.github.com>
2025-12-19 10:20:21 +0100

    NonTerminal instruction

--- src/LlvmirGenerator.java
+++ src/LlvmirGenerator.java
@@ -1,5 +1,4 @@
-import java.util.Map:
-
+import java.util.HashMap;
 
 /**
  * LLVM IR Generator from yaLcc parse tree
@@ -13,40 +12,67 @@ public class LlvmirGenerator {
     private StringBuilder llvmirCode;
 
     /** Map of varname and their identifier */
-    private Map<String, String> var;
+    private HashMap<String, String> var;
 
     /** unamed variable counter (temporary value) */
     private int unamedVarCounter = 0;
 
-
     public LlvmirGenerator(ParseTree parseTree) {
         this.parseTree = parseTree;
         this.llvmirCode = new StringBuilder();
-
-    }
-
-    public String generate() {
-        header();
-
-        return llvmirCode.toString();
+        this.var = new HashMap<>();
+        this.unamedVarCounter = 0;
     }
 
     private void header() {
-        String header = "; Generated LLVM IR code from ParserTree\n"
-                      + "Coucou <3\n";
+        String header =
+            "; Generated LLVM IR code from ParserTree\n" + "Coucou <3\n";
     }
 
-    public void newLine(String line, int indentLevel) {
+    private ParseTree getCodeBranch(ParseTree treeNode) {
+        for (ParseTree child : treeNode.getChildren()) {
+            if (child.getLabel().isNonTerminal()) {
+                return child;
+            }
+        }
+        return null;
+    }
+
+    public void generate(ParseTree treeNode) {
+        header();
+        newLine("define i32 @Prog() {", 0);
+        newLine("entry:", 1);
+
+        ParseTree code = getCodeBranch(treeNode);
+        if (code != null) {
+            code(code);
+        }
+
+        newLine("ret i32 0", 1);
+        newLine("}", 0);
+    }
+
+    /** Recursive method to chenerate code by reading tree */
+    private void code(ParseTree treeNode) {
+        for (ParseTree child : treeNode.getChildren()) {
+            if (child.getLabel().getValue() == NonTerminal.INSTRUCTION) {
+                newInstructions(child);
+            } else if (child.getLabel().getValue() == NonTerminal.CODE) {
+                code(child);
+            }
+        }
+    }
+
+    private void newLine(String line, int indentLevel) {
         llvmirCode.append(" ".repeat(indentLevel)).append(line).append("\n");
     }
-
 
     /** Retrieve the varId of a named var. Create it inplace if not existant */
     private String getOrNewI32(String varName) {
         if (!var.containsKey(varName)) {
             String varId = "%var_" + varName;
             var.put(varName, varId);
-            newLine(varId + " = alloca i32", 1);
+            newLine(varId + " = alloca i32", 2);
         }
         return var.get(varName);
     }
@@ -55,28 +81,57 @@ public class LlvmirGenerator {
     private String loadI32(String varName) {
         String varId = getOrNewI32(varName);
         String unamVarId = "%" + (unamedVarCounter++);
-        newLine(unamVarId + " = load i32, i32 * " + varId, 1);
+        newLine(unamVarId + " = load i32, i32 * " + varId, 2);
         return unamVarId;
     }
 
     /** store an unamed var into a named var. Create named var if non existant */
-    private void storeInNamVar(String varName, String unamVarId) {
+    private void storeInNamI32(String varName, String unamVarId) {
         String varId = getOrNewI32(varName);
-        newLine("store i32 " + unamVarId + ", i32* " + varId, 1);
+        newLine("store i32 " + unamVarId + ", i32* " + varId, 2);
     }
-
-    //TODO: Store constants ?
-
-
-
 
     /**
-    * Generate instruction recursively
-    * @param node
- */
-    private void generateInstructions(ParseTree node) {
+     * Generate instruction recursively
+     * @param node
+     */
+    private void newInstructions(ParseTree treeNode) {
+        ParseTree child = treeNode.getChildren().getFirst();
 
+        switch (child.getLabel().getValue()) {
+            case NonTerminal.ASSIGN:
+                newAssign(child);
+                break;
+            case NonTerminal.IF:
+                newIf(child);
+                break;
+            case NonTerminal.WHILE:
+                newWhile(child);
+                break;
+            case NonTerminal.OUTPUT:
+                newOutput(child);
+                break;
+            case NonTerminal.INPUT:
+                newInput(child);
+                break;
+            default:
+                throw new IllegalStateException(
+                    "Unexpected value: " + child.getLabel().getValue()
+                );
+        }
     }
 
+    private void newAssign(ParseTree treeNode) {}
 
+    private void newIf(ParseTree treeNode) {}
+
+    private void newWhile(ParseTree treeNode) {}
+
+    private void newOutput(ParseTree treeNode) {}
+
+    private void newInput(ParseTree treeNode) {}
+
+    private void newCond(ParseTree treeNode) {}
+
+    private void newExprArith(ParseTree treeNode) {}
 }
